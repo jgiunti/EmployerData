92107893442ab1757da080925d986730dffd91ac
Joe Giunti <jg4229@gmail.com>
2016-01-20 00:00:21 -0500

    Initial commit

---
+++ EDAW/EDAW/Contexts/JobExplrContext.cs
@@ -0,0 +1,29 @@
+﻿using EDAW.Data;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAW.Contexts
+{
+    class JobExplrContext
+    {
+        private IMongoDatabase explorerDB;
+        
+        public JobExplrContext(MongoClient client)
+        {
+            this.explorerDB = client.GetDatabase("EmployerData");
+        }
+
+        public IMongoCollection<BsonDocument> JobExplorers
+        {
+            get
+            {
+                return explorerDB.GetCollection<BsonDocument>("JobExplorers");
+            }
+        }
+    }
+}
---
+++ EDAW/EDAW/Database/Mongo.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAW.Database
+{
+    public class Mongo
+    {
+        public MongoClient client;
+
+        public Mongo()
+        {
+            MongoClientSettings settings = new MongoClientSettings();
+            settings.Server = new MongoServerAddress("localhost", 27017);
+
+            this.client = new MongoClient(settings);
+        }      
+    }
+}
